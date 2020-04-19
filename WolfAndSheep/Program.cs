207c05d5cc6e67c84f14e6b658bda7e2b054282e
André Furão Figueira <affigueir@gmail.com>
2020-04-19 21:59:22 +0100

    Commenting and report added, changed functions to uppercase

--- WolfAndSheep/Program.cs
+++ WolfAndSheep/Program.cs
@@ -4,14 +4,22 @@ namespace WolfAndSheep
 {
     class Program
     {
+        /// <summary>
+        /// Main method of the program. The program starts here.
+        /// </summary>
+        /// <param name="args">Command line arguments when running the program</param>
         static void Main(string[] args)
         {
+            /// <summary>
+            /// Declaring/Initializing Variables
+            /// </summary>
             int[,] board = new int[8, 8];
             short row, col, rowm, colm;
             playerName player = playerName.Wolf;
             string coord;
             bool gameOver = false;
 
+            //Fills the game board (bidimensional array) with empty positions
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
@@ -20,79 +28,123 @@ namespace WolfAndSheep
                 }
             }
 
+            /// <summary>
+            /// Placing the Sheep on the Game board
+            /// </summary>
             board[7, 0] = board[7, 2] = board[7, 4] = board[7, 6] = 2;
 
+            //Prints Welcome messages and instructions
             Console.WriteLine("Welcome to Wolf and Sheep!");
             Console.WriteLine("The rules are very simple.");
-            print_instructions();
+            PrintInstructions();
             Console.WriteLine("You can request this menu during "
                 + "the game by typing 'help'");
 
-            print_game(board);
+            ///Prints the Game board
+            PrintGame(board);
 
             Console.Write(
                 "Where do you wanna start as the Wolf? [1B] [1D] [1F] [1H]: ");
 
+            /// <summary>
+            /// Loop asking the Wolfs starting position until position is valid
+            /// </summary>
+            /// <value>true</value>
             do
             {
+                //Converts from ASCII to short int
                 coord = Console.ReadLine();
                 row = Convert.ToInt16(coord[0]);
                 col = Convert.ToInt16(coord[1]);
                 row -= 49;
                 col -= 65;
 
+                //Checks for help command, prints instructions
                 if (coord == "help")
                 {
-                    print_instructions();
-                    print_game(board);
+                    PrintInstructions();
+                    PrintGame(board);
                     Console.Write("Where do you wanna start? "
                     + "[1B] [1D] [1F] [1H]: ");
                 }
 
+                /// <summary>
+                /// Checks if the inserted position is valid, breaks the cycle if so
+                /// </summary>
                 else if (row == 0 & (col == 1 || col == 3 ||
                         col == 5 || col == 7)) break;
 
+                /// <summary>
+                /// If none of the above are true, this runs
+                /// </summary>
                 else Console.Write("Invalid position. Where do you wanna " +
                     "start? [1B] [1D] [1F] [1H]: ");
 
             } while (true);
 
+            /// <summary>
+            /// Saves the Wolfs position and placing him on board
+            /// </summary>
+            /// <value>Wolfs position</value>
             int[] wolfPos = { row, col };
 
             board[row, col] = 1;
 
             do
             {
-                print_game(board, player);
+                //Prints the Game board, asks and reads the move
+                PrintGame(board, player);
 
                 Console.Write("What's your move? ");
                 coord = Console.ReadLine();
 
-                if (coord == "help") print_instructions();
+                /// <summary>
+                /// Checks for help command, prints instructions
+                /// </summary>
+                /// <returns>Instructions content</returns>
+                if (coord == "help") PrintInstructions();
 
+                /// <summary>
+                /// Checking if its Player 1s turn and if the Input length is 2
+                /// </summary>
                 else if ((int)player == 1 & coord.Length == 2)
                 {
+                    //Converts Input from ASCII to int
                     row = Convert.ToInt16(coord[0]);
                     col = Convert.ToInt16(coord[1]);
                     row -= 49;
                     col -= 65;
 
+                    /// <summary>
+                    /// Checks if the desired move is a diagonal
+                    /// </summary>
+                    /// <param name="row">Desired row to move to</param>
+                    /// <param name="col">Desired column to move to</param>
+                    /// <returns>true/false</returns>
                     if (Math.Abs(wolfPos[0] - row) == 1 &
                         Math.Abs(wolfPos[1] - col) == 1 &
                         0 <= row & row <= 7 & 0 <= col & col <= 7)
                     {
+                        //Moves the wolf to the new position and empties the previous one
                         board[wolfPos[0], wolfPos[1]] = 0;
                         wolfPos[0] = row; wolfPos[1] = col;
-                        board[wolfPos[0], wolfPos[1]] = 1;
+                        board[wolfPos[0], wolfPos[1]] = 1; 
 
+                        //Changes to Player 2 (Sheep) 
                         player = playerName.Sheep;
                     }
-
+                    /// <summary>
+                    /// If the desired position is not a diagonal, prints this
+                    /// </summary>
                     else Console.Write("Invalid position.\n");
                 }
 
+                /// <summary>
+                /// Checks if its Player 2s turn and if the Input length is 5
+                /// </summary>
                 else if ((int)player == 2 & coord.Length == 5)
                 {
+                    //Converts Input from ASCII to int
                     row = Convert.ToInt16(coord[0]);
                     col = Convert.ToInt16(coord[1]);
                     row -= 49;
@@ -103,11 +155,14 @@ namespace WolfAndSheep
                     rowm -= 49;
                     colm -= 65;
 
+                    //Checks if the Input position is in the Games board range
                     if (0 <= row & row <= 7 & 0 <= col & col <= 7 &
                         0 <= rowm & rowm <= 7 & 0 <= colm & colm <= 7)
                     {
+                        //Checks if there is a Sheep on the Input position
                         if (board[row, col] == 2)
                         {
+                            //Checks if the desired move is a diagonal of the selected Sheep
                             if (row - rowm == 1 & Math.Abs(col - colm) == 1)
                             {
                                 board[row, col] = 0;
@@ -115,22 +170,37 @@ namespace WolfAndSheep
 
                                 player = playerName.Wolf;
                             }
-
+                            /// <summary>
+                            /// Prints if the desired move position is invalid
+                            /// </summary>
                             else Console.Write("Invalid position.\n");
                         }
-
+                        /// <summary>
+                        /// Prints if there is no Sheep in the Input position
+                        /// </summary>
                         else Console.Write("There is no sheep.\n");
                     }
+                    /// <summary>
+                    /// Prints if the Input is out of the Game board range
+                    /// </summary>
                     else Console.Write("Invalid Input.\n");
                 }
+                /// <summary>
+                /// Prints if the Input length is not the correct
+                /// </summary>
                 else Console.Write("Invalid Input.\n");
 
-                gameOver = game_check(board, wolfPos);
+                //Updates the game over variable
+                gameOver = GameCheck(board, wolfPos);
 
             } while (!gameOver);
         }
 
-        private static void print_game(int[,] table)
+        /// <summary>
+        /// Prints the Game board
+        /// </summary>
+        /// <param name="table">Game board</param>
+        private static void PrintGame(int[,] table)
         {
             Console.WriteLine("\n###############################\n");
 
@@ -142,13 +212,19 @@ namespace WolfAndSheep
 
                 for (int j = 0; j < 8; j++)
                 {
-                    Console.Write($"[{table[i, j]}]");
+                    Console.Write($"[{table[i, j]}]"); 
                 }
                 Console.WriteLine("");
             }
             Console.WriteLine("");
         }
-        private static void print_game(int[,] table, playerName player_name)
+        
+        /// <summary>
+        /// Prints the Game board
+        /// </summary>
+        /// /// <param name="table">Game board</param>
+        /// <param name="player_name">Current Players turn</param>
+        private static void PrintGame(int[,] table, playerName player_name)
         {
             Console.WriteLine("\n###############################\n");
 
@@ -162,14 +238,17 @@ namespace WolfAndSheep
 
                 for (int j = 0; j < 8; j++)
                 {
-                    Console.Write($"[{table[i, j]}]");
+                    Console.Write($"[{table[i, j]}]"); 
                 }
                 Console.WriteLine("");
             }
             Console.WriteLine("");
         }
 
-        private static void print_instructions()
+        /// <summary>
+        /// Prints Instructions ('help' command)
+        /// </summary>
+        private static void PrintInstructions()
         {
             Console.WriteLine("\nThe Wolf starts by choosing where he is "
                 + "positioned on the board.");
@@ -184,13 +263,19 @@ namespace WolfAndSheep
             Console.WriteLine("Good luck and have fun! :D\n");
         }
 
-        private static bool game_check(int[,] table, int[] wolf)
+        /// <summary>
+        /// Checks if the game is over or not and prints the winner when over, quits game loop
+        /// </summary>
+        /// <param name="table">Game board</param>
+        /// <param name="wolf">Wolfs position</param>
+        /// <returns>bool true/false</returns>
+        private static bool GameCheck(int[,] table, int[] wolf)
         {
             bool canMove = false;
 
             if (wolf[0] == 7)
             {
-                print_game(table);
+                PrintGame(table);
                 Console.WriteLine("The Wolf won the game!");
                 return true;
             }
@@ -219,7 +304,7 @@ namespace WolfAndSheep
 
                 if (!canMove)
                 {
-                    print_game(table);
+                    PrintGame(table);
                     Console.WriteLine("The Sheep won the game!");
                     return true;
                 }
--- WolfAndSheep/relatorio.md
+++ WolfAndSheep/relatorio.md
@@ -1,8 +1,50 @@
 # Wolf and Sheep
 
+André Figueira a21901435  
+João Matos a21901219  
+ Miguel Feliciano a21904115  
+
 ---
-André Figueira a21901435
 
-João Matos a21901219
+## Commits e Trabalho Realizado
 
-MiguelFeliciano a21904115
+1. **First Commit** - João Matos  
+   Criação do projeto.
+2. **Starting board design** - João Matos  
+   Design base do jogo, criação do tabuleiro de jogo.
+3. **First gameplay created** - André Figueira
+   Implementação do movimento dos jogadores.
+4. **functional game** - Miguel Feliciano
+   Implementação das regras de jogo, prevenção de alguns erros de Input e range de arrays.
+5. **Bugfixing and polishing** - João Matos  
+   Correção de bugs de Input e polishing do visual do jogo.
+6. **Commenting and report added, changed functions to uppercase** - André Figueira
+   Comentação do código e relátorio adicionados. Funções com iniciais maiúsculas.
+
+Apesar da divisão dos commits do trabalho pelo grupo, o trabalho foi realizado por todos os membros do grupo através de sessões de `Live Share` e partilha de ecrã através da aplicação `Discord`, assim todo o trabalho foi realizado em conjunto, de modo a facilitar a realização do mesmo.
+
+## [Repositório Github](https://github.com/JMatos1221/LP1_P1)
+
+## Arquitetura da Solução
+
+Como solução para este trabalho, pensámos em dividir o jogo por "etapas", ou seja, os vários procedimentos que o jogo iria ter. Foram criados vários métodos de forma a criar um `game loop`. 
+
+Primeiro, decidimos desenhar o tabuleiro de jogo, tendo decidido que seria melhor e mais simples como comandos de jogo ter um sistema de coordenadas, algo parecido à batalha naval, sendo de `1` a `8` para as colunas e de `A` a `H` para as filas.
+
+Desenhamos então o tabuleiro do jogo com um array bidimensional, 8x8, 8 filas e 8 colunas, sendo depois colocadas as ovelhas nas suas devidas posições. Feito isto, procedemos a pedir ao jogador, qual a posição onde queria colocar o Lobo.
+
+Feitas estas "etapas" chegamos ao ponto do `game loop` onde o jogo entra num ciclo `do while` com a condição da variável `gameOver` ser `false`, sendo que este `game loop` consiste nos movimentos dos jogadores e na verificação do fim do jogo. 
+
+## Métodos e enumerações
+
+- `enum` playerName - Enumeração consistente por dois valores, `Wolf (1)` e `Sheep (2)`, utilizada para gerir os turnos dos jogadores e representação do nome do jogador (Wolf ou Sheep) cada vez que o tabuleiro de jogo for imprimido, dando assim um melhor feedback.
+
+- `private static void` print_instructions() - Método criado para imprimir as instruções no início do jogo e sempre que o jogador chamar o comando `help`.
+
+- `private static void` print_game(`int`[,] table, `playerName` player_name) - Método utilizado para imprimir o tabuleiro do jogo e a quem pertence o turno. Este é um método implementado com overload, sendo que nem sempre recebe a variável `playerName` player, para se poder imprimir o tabuleiro sem imprimir a quem pertence o turno, como no início e fim do jogo.
+
+- `private static void` game_check(`int`[,] table, `int`[] wolf) - Método que verifica se o Lobo chegou ao fundo do tabuleiro (fila 8), atribuindo-lhe assim a vitória, ou se o Lobo tem jogadas possíveis, sendo que se não tiver, atribui-se a vitória às Ovelhas.
+  
+## Fluxograma  
+
+![Fluxograma](fluxograma.png)
